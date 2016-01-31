931f6a528b957561372ec7cd48e1e19f453df0e5
elisesellers <captain913@gmail.com>
2016-01-30 17:09:23 -0700

    original hint

---
+++ funnyWeekend/src/byui/CIT260/funnyWeekend/model/Hint.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.funnyWeekend.model;
+
+/**
+ *
+ * @author Lony
+ */
+public class Hint {
+    
+    private String gameWord;
+
+    public String getGameWord() {
+        return gameWord;
+    }
+
+    public void setGameWord(String gameWord) {
+        this.gameWord = gameWord;
+    }
+    
+    
+}
