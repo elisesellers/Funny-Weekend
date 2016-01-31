26d6c906253175cfa3203e9be16c86a5904c3aba
elisesellers <captain913@gmail.com>
2016-01-30 17:04:37 -0700

    Map original

---
+++ funnyWeekend/src/byui/CIT260/funnyWeekend/model/Map.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.funnyWeekend.model;
+
+/**
+ *
+ * @author Lony
+ */
+public class Map {
+    
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    private double currentScore;
+
+    public double getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(double currentScore) {
+        this.currentScore = currentScore;
+    }
+    
+    
+}
