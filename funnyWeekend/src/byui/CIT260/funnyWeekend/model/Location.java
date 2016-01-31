17ebc9ed4518184d341b1ee40bdb1ff686d64a02
elisesellers <captain913@gmail.com>
2016-01-30 17:26:18 -0700

    original location

---
+++ funnyWeekend/src/byui/CIT260/funnyWeekend/model/Location.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.funnyWeekend.model;
+
+/**
+ *
+ * @author Lony
+ */
+public class Location {
+    
+    private double coordinates;
+
+    public double getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(double coordinates) {
+        this.coordinates = coordinates;
+    }
+    
+    private double score;
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+    
+    
+}
