55c5ba50308a50191a5c976fe3f860107cbf9a3b
YuraVasiuk <Yura@Yura-PC>
2016-02-17 12:22:38 -0700

    2-17-2016. Here is our view layer.

---
+++ funnyWeekend/src/byui/CIT260/funnyWeekend/view/StartProgramView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.funnyWeekend.view;
+
+/**
+ *
+ * @author Yura
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+
+    public StartProgramView() {
+        
+        this.promptMessage = "\nPlease enter your name: ";
+        
+    }
+    
+}
