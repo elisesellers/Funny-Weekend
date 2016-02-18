8781d2c92209304e0de84ac1a58b7de4c49f8ecc
elisesellers <captain913@gmail.com>
2016-02-18 12:29:18 -0700

    2/18/2016 Changes to opening banner message

--- funnyWeekend/src/byui/CIT260/funnyWeekend/view/StartProgramView.java
+++ funnyWeekend/src/byui/CIT260/funnyWeekend/view/StartProgramView.java
@@ -5,6 +5,8 @@
  */
 package byui.CIT260.funnyWeekend.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Yura
@@ -25,16 +27,22 @@ public class StartProgramView {
         System.out.println(
         "\n*******************************************"
       + "\n*                                         *"
-      + "\nHi.  My name is Processor.  I live in this computer.  "
-      + "\nI would like to play a game with you.  "
-      + "\nIn this game you will be picking days and "
-      + "\nwriting the names of things and people you meet on those days.  "
-      + "\nYou input one word at a time.  "
-      + "\nI have my set of words, and the word you input has to match "
-      + "\nwith the one of mines.  No typos will be accepted!"
+      + "\nHello.  My name is Processor.  I live in this computer."
+      + "\nI would like to play a game with you."
+      + "\nIn this game you will be picking what days you would like to visit."
+      + "\nEach day has an activity for you to do."
+      + "\nOn some days you will need to write"
+      + "\nthe names of things and people you might meet on those days."
+      + "\nYou will type one word at a time.  "
+      + "\nI have my set of words, and the word you type has to match "
+      + "\nwith one of mine.  No typos will be accepted!"
       + "\nYou can call for help, but getting the help will cost you points."
-      + "\nYou can go through the days the way you like by using "
-      + "\nthe inputs from the location menu.  "
+      + "\nOther days will require you to solve a math problem."
+      + "\nI will tell you the problem and you will type an answer."
+      + "\nYour answer has to match mine."
+      + "\nYou can ask the wizard for help, but he will take some of your points."
+      + "\nYou can go through the days any way you like by using "
+      + "\nthe inputs from the map menu.  "
       + "\nYou can save and return to your game any time."
       + "\n*                                         *"
       + "\n*******************************************"  
