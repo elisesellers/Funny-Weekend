89d0f3ccb08ef876834f8b1782726f180ef0a16e
elisesellers <captain913@gmail.com>
2016-02-18 12:17:10 -0700

    2/18/2016 Developing the view layer

--- funnyWeekend/src/funnyweekend/FunnyWeekend.java
+++ funnyWeekend/src/funnyweekend/FunnyWeekend.java
@@ -10,6 +10,7 @@ import byui.CIT260.funnyWeekend.model.Describe;
 import byui.CIT260.funnyWeekend.model.MathAssignment;
 import byui.CIT260.funnyWeekend.model.Wizard;
 import byui.CIT260.funnyWeekend.model.Writing;
+import byui.CIT260.funnyWeekend.view.StartProgramView;
 
 /**
  *
@@ -22,38 +23,8 @@ public class FunnyWeekend {
      */
     public static void main(String[] args) {
         
-        // new objects and tests for them
-        Activity activityOne = new Activity();
-        
-        activityOne.setLocation(11);
-        String activityInfo = activityOne.toString();
-        System.out.println(activityInfo);
-        
-        Writing writingOne = new Writing();
-        
-        writingOne.setPlayerWord("Rexburg");
-        writingOne.setGameWord("Idaho Falls");
-        String writingInfo = writingOne.toString();
-        System.out.println(writingInfo);
-        
-        MathAssignment mathOne = new MathAssignment();
-        
-        mathOne.setPlayerResult(33);
-        mathOne.setGameResult(33);
-        String mathInfo = mathOne.toString();
-        System.out.println(mathInfo);
-        
-        Describe describeOne = new Describe();
-        
-        describeOne.setWordDescription("This will describe the game word");
-        String describeInfo = describeOne.toString();
-        System.out.println(describeInfo);
-        
-        Wizard wizardOne = new Wizard();
-        
-        wizardOne.setFormula("Celsius = (Fahrenheit - 32) * 5 / 9");
-        String wizardInfo = wizardOne.toString();
-        System.out.println(wizardInfo);
+       StartProgramView startProgramView = new StartProgramView();
+       startProgramView.displayStartProgramView();
     }
     
 }
