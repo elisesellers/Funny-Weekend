ac7701464c7a5eb7961376c18aa9dcf4f240382b
elisesellers <captain913@gmail.com>
2016-02-19 12:11:51 -0700

    2/19/2016 Display name

--- funnyWeekend/src/byui/CIT260/funnyWeekend/control/GameControl.java
+++ funnyWeekend/src/byui/CIT260/funnyWeekend/control/GameControl.java
@@ -5,10 +5,27 @@
  */
 package byui.CIT260.funnyWeekend.control;
 
+import byui.CIT260.funnyWeekend.model.Player;
+import funnyweekend.FunnyWeekend;
+
 /**
  *
  * @author Ellie
  */
 public class GameControl {
+
+    public static Player createPlayer(String name) {
+        
+        if (name == null){
+            return null;
+        }
+        
+        Player player = new Player();
+        player.setName(name);
+        
+        FunnyWeekend.setPlayer(player); //save the player
+        
+        return player;
+    }
     
 }
